7e774771251b11908044b06f8f1fb3df11dee3b4
Faccu21 <120684376+Faccu21@users.noreply.github.com>
2023-04-28 14:56:48 -0300

    Add files via upload

---
+++ Librerias/tribunas.js
@@ -0,0 +1,27 @@
+let tribunas = [
+    {
+      nombre: 'TribunaMessi',
+      precio: 100000,
+      id: 1,
+      img: './assets/SectorMessi.jpg' 
+    },
+    {
+      nombre: 'TribunaCR7',
+      precio: 80000,
+      id: 2,
+      img: './assets/SectorCR7.jpeg'
+    },
+    {
+      nombre: 'TribunaNey',
+      precio: 50000,
+      id: 3,
+      img: './assets/SectorNeymar.jpg'
+    },
+    {
+      nombre: 'TribunaMbappe',
+      precio: 50000,
+      id: 4,
+      img: './assets/SectorMbappe.png'
+    },
+    
+  ];
